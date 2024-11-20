f1ed8aa5c5a22eb94376b14718156d07ef767807
AriBlumstein <ayb1998@aol.com>
2024-11-20 19:31:51 +0200

    initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+/**
+ * Footer component.
+ * @returns {JSX.Element} Footer element.
+ */
+function Footer() {
+    return (
+        <footer>
+            <p>Copyright &copy; {new Date().getFullYear()}</p>
+        </footer>
+    );
+}
+
+
+export default Footer
